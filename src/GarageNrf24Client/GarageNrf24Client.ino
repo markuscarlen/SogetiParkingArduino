a8c68254d5e0382d0c0f830d3ea45f500f101ad0
Markus Carlen <markus.carlen@sogeti.se>
2016-04-25 16:14:23 +0200

    Added Nrf24lient project
    For warm garage sensors.

---
+++ src/GarageNrf24Client/GarageNrf24Client.ino
@@ -0,0 +1,71 @@
+#include "ParkingSpace.h"
+
+#define LED 2
+#define APP_VERSION(Major, Minor) Major << 4 + Minor
+
+#define APP_MAJOR 1
+#define APP_MINOR 0
+
+char stringToSend[160];
+
+ParkingSpace parkingSpace(99, 3, 4, 150);
+
+unsigned long ul_PreviousMillis = 0UL;
+unsigned long ul_Interval = 600000UL; //Interval 10min (10*60*1000)
+
+byte ErrorCode = 0;
+
+void setup()
+{
+  pinMode(LED, OUTPUT); //LED for showing sending to server.
+
+  InitSerialPort();
+  InitNrf24();
+
+  SendTimeIntervalStatusUpdate();
+  ul_PreviousMillis = millis();
+}
+
+void loop()
+{ 
+  delay(500);
+  if(parkingSpace.PingAndReturnStatusChanged()) SendData(parkingSpace.GetSpaceNumber(), parkingSpace.IsFree());
+
+  if(IntervalTimerElapsed()) SendTimeIntervalStatusUpdate();
+
+}
+
+void SendData(int spaceNr, bool isFree)
+{
+  Nrf24SendData(spaceNr, isFree, ErrorCode);
+  ErrorCode = 0;
+}
+
+void SendTimeIntervalStatusUpdate()
+{
+  if(parkingSpace.PingAndReturnStatusChanged()) SendData(parkingSpace.GetSpaceNumber(), parkingSpace.IsFree());
+}
+
+bool IntervalTimerElapsed()
+{  unsigned long ul_CurrentMillis = millis();
+  if( ul_CurrentMillis - ul_PreviousMillis > ul_Interval)
+  {
+    ul_PreviousMillis += ul_Interval;
+    return true;
+  }
+  return false;    
+}
+
+
+void InitSerialPort()
+{
+    Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println(F("Starting..."));
+}
+
+
+
+
---
+++ src/GarageNrf24Client/NRF24.ino
@@ -0,0 +1,75 @@
+#include <RH_NRF24.h>
+#include <SPI.h>
+
+#define PROTOCOL_VERSION 1
+
+RH_NRF24 nrf24;
+
+
+
+void InitNrf24()
+{
+  if (!nrf24.init())Serial.println("init failed");
+
+  if (!nrf24.setChannel(1))Serial.println("setChannel failed");
+  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm)) Serial.println("setRF failed");   
+  //if(!nrf24.sleep()) Serial.println("setSleep failed");
+}
+
+
+bool Nrf24SendData(unsigned int parkingSpace, bool isFree, byte errorCode)
+{
+  byte data[8];
+  byte timeOut = 0; 
+  
+  data[7] = PROTOCOL_VERSION;
+  data[6] = APP_VERSION(APP_MAJOR, APP_MINOR);
+  data[5] = parkingSpace >> 8;
+  data[4] = parkingSpace %256;
+  data[3] = isFree;
+  data[2] = 100; //Reserved
+  data[1] = 100;
+  data[0] = 100; //Battery Voltage
+
+  Serial.println("Sending nrf24...");
+  while(1)
+  {
+    nrf24.send(data, sizeof(data));
+    
+    nrf24.waitPacketSent() ? Serial.println("Message sent.") : Serial.println("Message not set.");
+  
+    Serial.println("Waiting for Response...");
+    if (nrf24.waitAvailableTimeout(500))
+    { Serial.println("is available...");
+      // Should be a reply message for us now 
+      uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+      uint8_t len = sizeof(buf);
+      if (nrf24.recv(buf, &len))
+      {
+        Serial.print("got reply: ");
+        Serial.println((char*)buf);
+        SendToSerialPort(buf, sizeof(buf));
+        break;
+      }
+      else
+      {
+        Serial.println("recv failed");
+      }
+    }
+    Serial.println("Resending data...");
+  }
+  nrf24.sleep();
+  return true;
+}
+
+void SendToSerialPort(byte* b, byte len)
+{
+  while(len > 0)
+  {
+    Serial.print(len);
+    Serial.print(": ");
+    Serial.print(b[len--]);
+    Serial.print(" | ");
+  }
+  Serial.println("");
+}
